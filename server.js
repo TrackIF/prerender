9e44bd4ca26f948a0a4890e128f5b5f5b633ce61
Brandon Moser <brandon@trackif.com>
2015-04-08 16:32:43 -0500

    Add defaults from old TIF prerender server

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ var server = prerender({
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    followRedirect: process.env.FOLLOW_REDIRECTS || true,
+    waitAfterLastRequest: process.env.REQUEST_WAIT || 2000,
+    jsTimeout: process.env.JS_TIMEOUT || 20000,
+    phantomArguments: [
+      "--load-images=false",
+      "--ignore-ssl-errors=true",
+      "--ssl-protocol=tlsv1",
+      "--disk-cache=true",
+      "--max-disk-cache-size=1048576"
+    ]
 });
 
 
