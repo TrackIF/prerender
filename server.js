cb5eee300b3ed545b46ebd0c6b6113289f1cc5b5
Brandon Moser <brandon@trackif.com>
2015-04-08 16:36:19 -0500

    Fix typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT,
     followRedirect: process.env.FOLLOW_REDIRECTS || true,
     waitAfterLastRequest: process.env.REQUEST_WAIT || 2000,
     jsTimeout: process.env.JS_TIMEOUT || 20000,
