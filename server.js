ee03cdc7b7422a786d24e2dcf9be7fa9995d6d2c
Brandon Moser <brandon@trackif.com>
2015-04-09 13:53:36 -0500

    Remove caching options

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ var server = prerender({
     waitAfterLastRequest: process.env.REQUEST_WAIT || 2000,
     jsTimeout: process.env.JS_TIMEOUT || 20000,
     phantomArguments: [
-      "--load-images=false",
       "--ignore-ssl-errors=true",
-      "--ssl-protocol=tlsv1",
-      "--disk-cache=true",
-      "--max-disk-cache-size=1048576"
+      "--ssl-protocol=tlsv1"
     ]
 });
 
