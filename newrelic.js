3092fb8d0e6ff68f913d8cd4ba8594c24f4ebafc
Brandon Moser <brandon@trackif.com>
2015-04-13 17:06:23 -0500

    Add New Relic monitoring

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['prerender'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'e5638ead0697d0eda7a680c9c3e9f9ed0eae69e9',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'trace'
+  }
+}
